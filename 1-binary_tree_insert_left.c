f71bae6416eac82e57816a3e613e8c42af0d8682
dev0nat <natdev01@tuta.io>
2023-05-31 00:22:27 -0700

    betty style fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - function inserts a node as the left-child of another node
+ * binary_tree_insert_left - function insert a node at the left of another node
  * @parent: a pointer to the node to insert the left child in
  * @value: the value to store in the new node
  *
  * Return: If parent is NULL or an error occurs - NULL.
- * 	   Otherwise - a pointer to the new node.
+ *		Otherwise - a pointer to the new node.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
