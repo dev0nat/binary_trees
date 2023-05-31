e69b10317323abbddd0cc82f7e84f853c5ed8278
dev0nat <natdev01@tuta.io>
2023-05-31 01:40:03 -0700

    fixed comment word limit per line

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right -Function insert a node at the right of another node
+ * binary_tree_insert_right - inserts a node at the right side of another node
  * @parent: a pointer to the node to insert the right child in
  * @value: the value to store in the new node
  *
